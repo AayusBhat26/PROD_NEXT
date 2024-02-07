eb5f015bc507de8df0d8f9d546b77dd67b659403
AayusBhat26 <100667276+AayusBhat26@users.noreply.github.com>
2024-02-07 18:33:10 +0530

    kicking the non admin users from hub

--- components/modals/members-modal.tsx
+++ components/modals/members-modal.tsx
@@ -45,6 +45,24 @@ export const MembersModal = () => {
   const [loadingId, setLoadingId] = useState('');
   const isModalOpen = isOpen && type === "members";
   const { server } = data as { server: HubWithMembersWithProfiles }
+  const onKick = async (memberId:string)=>{
+    try {
+      setLoadingId(memberId);
+      const url = qs.stringifyUrl({
+        url:`/api/members/${memberId}`, query:{
+          serverId: server?.id
+        }
+      });
+      const response = await axios.delete(url);
+      router.refresh();
+      onOpen('members', {server:response.data})
+    } catch (error) {
+      console.log("onKick", error);
+      
+    }finally{
+      setLoadingId('')
+    }
+  }
   const onRoleChange = async (memberId: string, role: MemberRole) => {
     try {
       setLoadingId(memberId);
@@ -127,7 +145,7 @@ export const MembersModal = () => {
                               </DropdownMenuPortal>
                             </DropdownMenuSub>
                             <DropdownMenuSeparator />
-                              <DropdownMenuItem>
+                              <DropdownMenuItem onClick={()=>onKick(member.id)}>
                                 <Frown className="h-4 w-4 mr-2"/>
                                 KICK 
                               </DropdownMenuItem>
