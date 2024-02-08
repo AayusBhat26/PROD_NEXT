849945c2ac229ec1d8952dda00968ad9be163d1d
AayusBhat26 <100667276+AayusBhat26@users.noreply.github.com>
2024-02-08 23:56:31 +0530

    modified the next.config.js for image domain

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: ["utfs.io", "uploadthing.com"],
+    // remotePatterns: ["utfs.io", "uploadthing.com"],
+    remotePatterns:[
+      {
+        protocol:'https', hostname:'uploadthing.com', pathname:'*'
+      }
+    ]
   },
 };
 
