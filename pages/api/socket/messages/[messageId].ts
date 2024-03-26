43ac5a6a24dfa790b3fe621555fdfff26e2d3aaa
AayusBhat26 <100667276+AayusBhat26@users.noreply.github.com>
2024-03-27 00:27:32 +0530

    fixing production errors

--- pages/api/socket/messages/[messageId].ts
+++ pages/api/socket/messages/[messageId].ts
@@ -1,73 +1,70 @@
+import { NextApiRequest } from "next";
+import { MemberRole } from "@prisma/client";
+
+import { NextApiResponseServerIo } from "@/types";
 import { currentProfilePages } from "@/lib/current-profile-pages";
 import { db } from "@/lib/db";
-import { NextApiResponseServerIo } from "@/types";
-import { MemberRole } from "@prisma/client";
-import { error } from "console";
-import { NextApiRequest } from "next";
 
 export default async function handler(
       req: NextApiRequest,
-      res: NextApiResponseServerIo
+      res: NextApiResponseServerIo,
 ) {
       if (req.method !== "DELETE" && req.method !== "PATCH") {
-            return res.status(405).json({
-                  error: "Method not supported"
-            })
+            return res.status(405).json({ error: "Method not allowed" });
       }
+
       try {
             const profile = await currentProfilePages(req);
             const { messageId, serverId, channelId } = req.query;
             const { content } = req.body;
+
             if (!profile) {
-                  return res.status(401).json({
-                        error: "authentication failed"
-                  })
+                  return res.status(401).json({ error: "Unauthorized" });
             }
+
             if (!serverId) {
-                  return res.status(400).json({
-                        error: "server search failed"
-                  })
+                  return res.status(400).json({ error: "Server ID missing" });
             }
+
             if (!channelId) {
-                  return res.status(401).json({
-                        error: "channel serarch failed"
-                  })
+                  return res.status(400).json({ error: "Channel ID missing" });
             }
+
             const server = await db.server.findFirst({
                   where: {
                         id: serverId as string,
                         members: {
                               some: {
-                                    profileId: profile.id
+                                    profileId: profile.id,
                               }
                         }
                   },
                   include: {
-                        members: true
+                        members: true,
                   }
             })
+
             if (!server) {
-                  return res.status(401).json({
-                        error: "server search failed"
-                  })
+                  return res.status(404).json({ error: "Server not found" });
             }
 
             const channel = await db.channel.findFirst({
                   where: {
                         id: channelId as string,
-                        serverId: serverId as string
-                  }
-            })
+                        serverId: serverId as string,
+                  },
+            });
+
             if (!channel) {
-                  return res.status(401).json({
-                        error: "channel search failed"
-                  })
-            } const member = server.members.find((member) => member.profileId === profile.id)
-            if (!member) {
-                  return res.status(401).json({
-                        error: "member search failed"
-                  })
+                  return res.status(404).json({ error: "Channel not found" });
             }
+
+            const member = server.members.find((member) => member.profileId === profile.id);
+
+            if (!member) {
+                  return res.status(404).json({ error: "Member not found" });
+            }
+
             let message = await db.message.findFirst({
                   where: {
                         id: messageId as string,
@@ -81,20 +78,20 @@ export default async function handler(
                         }
                   }
             })
+
             if (!message || message.deleted) {
-                  return res.status(404).json({
-                        error: "message search failed"
-                  })
+                  return res.status(404).json({ error: "Message not found" });
             }
+
             const isMessageOwner = message.memberId === member.id;
             const isAdmin = member.role === MemberRole.ADMIN;
             const isModerator = member.role === MemberRole.MODERATOR;
             const canModify = isMessageOwner || isAdmin || isModerator;
+
             if (!canModify) {
-                  return res.status(401).json({
-                        error: "Current Member is unauthroized to perform this action"
-                  })
+                  return res.status(401).json({ error: "Unauthorized" });
             }
+
             if (req.method === "DELETE") {
                   message = await db.message.update({
                         where: {
@@ -102,11 +99,9 @@ export default async function handler(
                         },
                         data: {
                               iamgeUrl: null,
-                              content: "Owner or admin or moderator has deleted this message",
+                              content: "This message has been deleted.",
                               deleted: true,
                         },
-
-
                         include: {
                               member: {
                                     include: {
@@ -116,18 +111,18 @@ export default async function handler(
                         }
                   })
             }
+
             if (req.method === "PATCH") {
                   if (!isMessageOwner) {
-                        return res.status(401).json({
-                              error: "you are not authorized to update this message"
-                        })
+                        return res.status(401).json({ error: "Unauthorized" });
                   }
+
                   message = await db.message.update({
                         where: {
                               id: messageId as string,
                         },
                         data: {
-                              content
+                              content,
                         },
                         include: {
                               member: {
@@ -138,13 +133,14 @@ export default async function handler(
                         }
                   })
             }
+
             const updateKey = `chat:${channelId}:messages:update`;
+
             res?.socket?.server?.io?.emit(updateKey, message);
+
             return res.status(200).json(message);
       } catch (error) {
-            console.log("inside the messageid,", error)
-            return res.status(500).json({
-                  error: "Internal Server Error"
-            })
+            console.log("[MESSAGE_ID]", error);
+            return res.status(500).json({ error: "Internal Error" });
       }
 }
--- pages/api/socket/messages/index.ts
+++ pages/api/socket/messages/index.ts
@@ -1,35 +1,41 @@
-import { currentProfilePages } from "@/lib/current-profile-pages";
-import { NextApiResponseServerIo } from "@/types";
 import { NextApiRequest } from "next";
-import { db } from "@/lib/db"
+
+import { NextApiResponseServerIo } from "@/types";
+import { currentProfilePages } from "@/lib/current-profile-pages";
+import { db } from "@/lib/db";
+
 export default async function handler(
       req: NextApiRequest,
-      res: NextApiResponseServerIo
+      res: NextApiResponseServerIo,
 ) {
       if (req.method !== "POST") {
-            return res.status(405).json({ error: "Method not supported" })
+            return res.status(405).json({ error: "Method not allowed" });
       }
+
       try {
             const profile = await currentProfilePages(req);
             const { content, fileUrl } = req.body;
-            const { hubsId, channelId } = req.query;
+            const { serverId, channelId } = req.query;
+
             if (!profile) {
-                  return res.status(401).json({
-                        error: 'Unauthorized access'
-                  })
+                  return res.status(401).json({ error: "Unauthorized" });
             }
-            if (!hubsId) {
-                  return res.status(400).json({ error: 'Hubs ID not found' })
+
+            if (!serverId) {
+                  return res.status(400).json({ error: "Server ID missing" });
             }
+
             if (!channelId) {
-                  return res.status(400).json({ error: 'channel ID not found' })
+                  return res.status(400).json({ error: "Channel ID missing" });
             }
+
             if (!content) {
-                  return res.status(400).json({ error: 'content not found' })
+                  return res.status(400).json({ error: "Content missing" });
             }
-            const hub = await db.server.findFirst({
+
+            const server = await db.server.findFirst({
                   where: {
-                        id: hubsId as string,
+                        id: serverId as string,
                         members: {
                               some: {
                                     profileId: profile.id
@@ -37,45 +43,54 @@ export default async function handler(
                         }
                   },
                   include: {
-                        members: true
+                        members: true,
                   }
-            })
-            if (!hub) {
-                  return res.status(404).json({ message: 'hub not foumd' });
+            });
+
+            if (!server) {
+                  return res.status(404).json({ message: "Server not found" });
             }
+
             const channel = await db.channel.findFirst({
                   where: {
                         id: channelId as string,
-                        serverId: hubsId as string,
+                        serverId: serverId as string,
                   }
-            })
+            });
+
             if (!channel) {
-                  return res.status(404).json({ message: 'Channel not found' });
+                  return res.status(404).json({ message: "Channel not found" });
             }
-            const member = hub.members.find((member) => member.profileId === profile.id);
+
+            const member = server.members.find((member) => member.profileId === profile.id);
+
             if (!member) {
-                  return res.status(404).json({ message: 'member not found' });
+                  return res.status(404).json({ message: "Member not found" });
             }
 
             const message = await db.message.create({
                   data: {
-                        //todo: to check if the fileurl would be accepted by the imageurl without mentioning it as a string.
-                        content, iamgeUrl: fileUrl, channelId: channelId as string,
+                        content,
+                        iamgeUrl: fileUrl,
+                        channelId: channelId as string,
                         memberId: member.id,
                   },
                   include: {
                         member: {
                               include: {
-                                    profile: true
+                                    profile: true,
                               }
                         }
                   }
-            })
+            });
+
             const channelKey = `chat:${channelId}:messages`;
-            res?.socket?.server?.io?.emit(channelKey, message)
-            return res.status(200).json(message)
+
+            res?.socket?.server?.io?.emit(channelKey, message);
+
+            return res.status(200).json(message);
       } catch (error) {
-            console.log('[messagees api ]', error)
-            return res.status(500).json({ message: "Internal error" })
+            console.log("[MESSAGES_POST]", error);
+            return res.status(500).json({ message: "Internal Error" });
       }
 }
