ad61409603139e72d767a5a1c23498426a29daa1
AayusBhat26 <100667276+AayusBhat26@users.noreply.github.com>
2024-03-26 19:49:49 +0530

    update and fix

--- components/hooks/use-origin.ts
+++ components/hooks/use-origin.ts
@@ -1,15 +1,15 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
-export const useOrigin = ()=>{
-      const [mounted, setMounted]=useState(false);
-      useEffect(()=>{
-            setMounted(true);
-      }, []);
+export const useOrigin = () => {
+      const [origin, setOrigin] = useState(null);
+      const [mounted, setMounted] = useState(false);
 
-      const origin = typeof window !=='undefined'  && window.location.origin ? window.location.origin : "";
+      useEffect(() => {
+            if (!mounted) {
+                  setOrigin(window.location.origin || "");
+                  setMounted(true);
+            }
+      }, [mounted]);
 
-      if(!mounted){
-            return "";
-      }
       return origin;
-}
+};
